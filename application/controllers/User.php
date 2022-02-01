ee343e8f0758969f6ad4cbe642fc5361f5eed732
giannurwana19 <giannurwana19@gmail.com>
2022-02-01 15:46:46 +0700

    form valdation user data

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -18,6 +18,24 @@ class User extends CI_Controller
 
 	public function add()
 	{
-		$this->template->load('template', 'user/user_form_add');
+		$this->form_validation->set_rules('name', 'Name', 'required|min_length[3]');
+		$this->form_validation->set_rules('username', 'Username', 'required|min_length[5]|is_unique[users.username]');
+		$this->form_validation->set_rules('password', 'Password', 'required');
+		$this->form_validation->set_rules('password_confirmation', 'Password', 'required|matches[password]', ['matches' => '%s tidak sesuai dengan password']);
+		$this->form_validation->set_rules('level', 'Level', 'required');
+
+		$this->form_validation->set_message('required', '%s masih kosong, silahkan isi');;
+		$this->form_validation->set_message('min_length', '{field} minimal {param} karakter');;
+		$this->form_validation->set_message('is_unique', 'Nama sudah ada, {field} harus unik');;
+
+		if ($this->form_validation->run() == false) {
+			$this->template->load('template', 'user/user_form_add');
+		} else {
+			echo "<pre>";
+			print_r($this->input->post());
+			echo "</pre>";
+			die;
+			echo 'proses simpan data user';
+		}
 	}
 }
--- application/views/user/user_form_add.php
+++ application/views/user/user_form_add.php
@@ -1,1 +1,65 @@
-Add User
+<section class="content-header">
+	<h1>
+		User
+		<small>Pengguna</small>
+	</h1>
+	<ol class="breadcrumb">
+		<li><a href="#"><i class="fa fa-dashboard"></i></a></li>
+		<li class="active">Users</li>
+	</ol>
+</section>
+
+<!-- Main content -->
+<section class="content">
+	<div class="box">
+		<div class="box-header">
+			<h3 class="box-title">Add User</h3>
+			<div class="pull-right">
+				<a href="<?= site_url('user'); ?>" class="btn btn-primary btn-sm"><i class="fa fa-undo"></i> Back</a>
+			</div>
+		</div>
+		<div class="box-body">
+			<div class="row">
+				<div class="col-md-4 col-md-offset-4">
+					<?php echo validation_errors(); ?>
+					<form action="" method="POST">
+						<div class="form-group">
+							<label for="name">Name *</label>
+							<input type="text" name="name" value="<?= set_value('name') ?>" class="form-control" id="name" autofocus>
+							<?= form_error('name') ?>
+						</div>
+						<div class="form-group">
+							<label for="username">Username *</label>
+							<input type="text" name="username" value="<?= set_value('username') ?>" class="form-control" id="username">
+						</div>
+						<div class="form-group">
+							<label for="password">Password *</label>
+							<input type="password" name="password" class="form-control" id="password">
+						</div>
+						<div class="form-group">
+							<label for="password_confirmation">Password Confirmation *</label>
+							<input type="password" class="form-control" name="password_confirmation" id="password_confirmation">
+						</div>
+						<div class="form-group">
+							<label for="address">Address *</label>
+							<textarea name="address" class="form-control" id="address" cols="30" rows="2"><?= set_value('address') ?></textarea>
+						</div>
+						<div class="form-group">
+							<label for="level">Level</label>
+							<select name="level" class="form-control" id="level">
+								<option value="">Pilih</option>
+								<option value="1">Admin</option>
+								<option value="2">Kasir</option>
+							</select>
+						</div>
+
+						<div class="form-group">
+							<button type="submit" class="btn btn-success">Save</button>
+							<button type="reset" class="btn btn-warning">Reset</button>
+						</div>
+					</form>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
